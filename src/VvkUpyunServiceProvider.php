d62ab584331b67805d189e6695065733e0ff62bc
vvk <845176725@qq.com>
2020-01-17 15:12:29 +0800

    init

---
+++ src/VvkUpyunServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Vvk\Upyun\FileSystem;
+
+use Illuminate\Support\ServiceProvider;
+
+class VvkUpyunServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
